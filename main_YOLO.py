efa67521fc818422c116855de1e6e6eb17ebc5ad
RainierJared <botor.jared@outlook.com>
2024-07-03 13:24:53 +1200

    Added ability to render circles

--- main_YOLO.py
+++ main_YOLO.py
@@ -1,18 +1,17 @@
 from ultralytics import YOLO
 import cv2
+import numpy as np
 import sys
 
-turnedOn = False
 
-#Loading pretrained model
-model = YOLO('./open_pose_project/Industrial_Project_HAR_for_Jetson_Nano/model/yolov8n-pose.pt')
-
-model.export(format='ncnn')    #creates './yolov8n_ncnn_model'
+# #Loading pretrained model **ONLY NEED TO RUN IT ONCE AT THE START TO CREATE THE LIGHTWEIGHT MODEL
+# model = YOLO('./open_pose_project/Industrial_Project_HAR_for_Jetson_Nano/model/yolov8n-pose.pt')
+# model.export(format='ncnn')    #creates './yolov8n_ncnn_model'
 
 ncnn_model=YOLO('./open_pose_project/Industrial_Project_HAR_for_Jetson_Nano/model/yolov8n-pose_ncnn_model')
 
-#camera = cv2.VideoCapture("./open_pose_project/Industrial_Project_HAR_for_Jetson_Nano/test_video/walking.mp4")     #For videos when training the classifier
-camera = cv2.VideoCapture(0)        #For live feed
+camera = cv2.VideoCapture("./open_pose_project/Industrial_Project_HAR_for_Jetson_Nano/test_video/walking.mp4")     #For videos when training the classifier
+#camera = cv2.VideoCapture(0)        #For live feed
 
 if (camera.isOpened() == False):
     print("Error opening camera")
@@ -25,6 +24,7 @@ while camera.isOpened():
         keyPoints = results[0].keypoints.data
         for kpts in keyPoints:
             for pts in kpts:
+                # cv2.circle(frame, (int(pts[0]), int(pts[1])), 4, (0,255,0), -1)       #For drawing the keypoints
                 print(f'X: {pts[0]}, Y: {pts[1]}, Z: {pts[2]}')
 
         cv2.imshow("Camera",frame)
